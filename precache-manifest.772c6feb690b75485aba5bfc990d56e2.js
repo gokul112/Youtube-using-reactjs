d9aacac5116c05460f1fd497b54e72fe1e99071a
gokul raj <gokul.raj@gigamon.com>
2020-06-08 17:21:10 +0530

    Updates

---
+++ precache-manifest.772c6feb690b75485aba5bfc990d56e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd5bc784ee14429c3d38685f11aa1df4",
+    "url": "/Youtube-using-reactjs/index.html"
+  },
+  {
+    "revision": "007b272758333613658b",
+    "url": "/Youtube-using-reactjs/static/css/main.6794d995.chunk.css"
+  },
+  {
+    "revision": "96bfe954af8786f2e1b9",
+    "url": "/Youtube-using-reactjs/static/js/2.5bea5749.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Youtube-using-reactjs/static/js/2.5bea5749.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "007b272758333613658b",
+    "url": "/Youtube-using-reactjs/static/js/main.76a278b6.chunk.js"
+  },
+  {
+    "revision": "54d633b627bda4a17f4b",
+    "url": "/Youtube-using-reactjs/static/js/runtime-main.94d26f8a.js"
+  }
+]);
